06e7ef33636213bad552caea15f8f2689bb1a18b
SvM79 <martinmarvidsson@duck.com>
2025-04-01 22:39:14 +0200

    Add /status route for Render test

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,6 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return "Hello, Flask + Render!"
+@main.route('/status')
+def status():
+    return "CI/CD to Render is working!"
